4f38ef1e88fd57a2ac5599c1eac870f4881a050e
Aniket Bhanawase <axlr88995@gmail.com>
2018-11-11 15:37:47 +0530

    new components with service code generated with ng g s data(service_name)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path: '',
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
